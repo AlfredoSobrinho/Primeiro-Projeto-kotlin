2f9d7d9ca2669b77d0973fb5edf1a30db07cdeb0
Alfredo <freddevs0001@gmail.com>
2022-02-21 12:54:14 -0300

    Primeiro Projeto

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,33 @@
+fun main(){
+   
+    println("Alfredinho: bom dia galera")
+    println("Marcelinho: eai jovem, bom dia")
+    println("Marinhinha: bom dia ")
+    println("Alfredinho: estou comendo queijo de cabra com café")
+    println("Marininha: numca nem vi")
+    println("marcelinho: eu comi uma vez em ibiuna")
+    println("Alfredinho: eu ja morei em ibiuna")
+    println("marininha: Onde fica ibiuna?")
+    println("Geovaninha: Bom dia gente!! ")
+    println("Alfredinho: Bom dia")
+    println("Marcelinho: bom dia gi!")
+    println("Marininha: Bom dia!!!")
+    println("Alfredinho: Marininha ibiuna fica no interior de são paulo")
+    println("Giovaninha: Ja comi queijo de cabra na casa da mimnha avó, muito bom")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
